3e7684ac516e71284d34d4942938592578fe1bce
govindyadav-14 <govindyadav11115@gmail.com>
2026-05-19 12:27:37 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log(1>2);
+console.log("1">2);
+console.log(null>2);
+console.log(null>0);
+console.log(null==0);//== alag tarh se kaam krta 
+// or < or >== or < ye are comparison operator alag tarh se kaam krte h 
+//== me null ko integer me nji krte convert or sab me krte h 
+console.log(null>=0);
+console.log(null>-2);
+//=== also compare datatype thats why it will give "0"==0 fasle
