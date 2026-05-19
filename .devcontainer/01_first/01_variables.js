6c50632df4a36acaecef74aad497dedf1087f11d
govindyadav-14 <govindyadav11115@gmail.com>
2026-05-19 11:28:30 +0000

    variables

---
+++ .devcontainer/01_first/01_variables.js
@@ -0,0 +1,13 @@
+const accountID=144552
+let accountMail="govindyadav@google.accountMail"
+var accountPass="1115"
+let name;//means undefined
+accountCity="jaipur"
+// accountID=2 not allowed
+accountMail="gyadav@g.com"
+accountPass="00999"
+accountCity="delhi"
+/*
+dont use var 
+*/
+console.table([accountID, name ,accountMail,accountPass,accountCity])
