9c281746145d4022608f7ed0155186893fcc033a
govindyadav-14 <govindyadav11115@gmail.com>
2026-05-19 11:57:46 +0000

    converisons

---
+++ .devcontainer/01_basics/conversions.js
@@ -0,0 +1,24 @@
+// let num=33;
+// console.log(typeof num);
+// console.log(num);
+
+// num=String(num);
+// console.log(typeof num);
+// console.log(num);
+//Number()
+let kuch;
+console.log(kuch);//undefined
+console.log(typeof kuch);//undefined
+let some=null;
+
+console.log(some);//null
+console.log(typeof some)//object
+
+let num=Number(some);
+let nnnum=Number(kuch);
+console.log(num);
+console.log(nnnum);
+console.log(+true);
+let x=Boolean(-1)
+console.log(x)
+
