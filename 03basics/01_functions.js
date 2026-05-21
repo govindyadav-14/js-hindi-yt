0149a1cecb04826ac1f2366fdf955eb82412db76
govindyadav-14 <govindyadav11115@gmail.com>
2026-05-21 20:32:34 +0000

    functionIntro

---
+++ 03basics/01_functions.js
@@ -0,0 +1,19 @@
+// function saymyname(){
+//     console.log("Govind Yadav")
+// }
+// saymyname()
+// // function add(number1,number2){// parameters
+// //     console.log(number1+number2);
+// // }
+// function add(number1,number2){// parameters
+//     let result=number1+number2
+//     return result
+    
+// }
+// // add(2,3)//arguments
+// console.log(add(4,5));
+
+function login(name="papa"){
+    return `${name} is just logged in`;
+}
+console.log(login("govind"))
